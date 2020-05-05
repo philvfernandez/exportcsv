64ab47d8d3f52b3673783e2b448454f62bdf49dc
Phil Fernandez <philvfernandez@gmail.com>
2020-05-05 17:10:11 -0600

    Added updates for adding a header to output csv file.  Also added update for using the latest export file by last modified update.

--- src/main/java/com/dev/kratos/exportcsv/ExportcsvApplication.java
+++ src/main/java/com/dev/kratos/exportcsv/ExportcsvApplication.java
@@ -1,17 +1,14 @@
 package com.dev.kratos.exportcsv;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.monitorjbl.xlsx.StreamingReader;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sun.rmi.runtime.Log;
 
 import java.io.*;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.*;
 
 @SpringBootApplication
 @Slf4j
@@ -21,11 +18,40 @@ public class ExportcsvApplication {
 
         SpringApplication.run(ExportcsvApplication.class, args);
 
-        File file = new File("map_ALL_export_20200430110001_file.xlsx");
-        File csvFile = new File("C:/Edit/Dev/map_export.csv");
+        FilenameFilter map_export_filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                if(name.contains("map_ALL_export_")) {
+                    //filter all map_export files
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+
+        String dirPath = "C:/Edit/Dev";
+        File dir = new File(dirPath);
+        File[] files = dir.listFiles(map_export_filter);
+
+        //Returns a list of file names in sorted order by last modified date
+        if(files != null) {
+            Arrays.sort(files, Comparator.comparing(File::lastModified));
+        }
+
+        if(files.length == 0) {
+            log.info("There is no map export files");
+        }
+
+        //File file = new File("map_ALL_export_20200430110001_file.xlsx");
+        File file = new File(files[files.length-1].toString());
+        //Get only the base file name w/o extension
+        String theFile = FilenameUtils.getBaseName(file.toString());
+
+        //Output CSV file name
+        File csvFile = new File("C:/Edit/Dev/"+theFile+".csv");
         BufferedWriter bw = null;
 
-        //InputStream inputStream = null;
         try {
             /*
             Makes sure that the file gets created if it is not present at the specified location
@@ -52,8 +78,11 @@ public class ExportcsvApplication {
             while(rowIterator.hasNext()) {
                 Row row = rowIterator.next();
                 int rVal = row.getRowNum();
+                int i = 1;
 
-                if(rVal > 3) {
+                //write out header
+
+                if(rVal >= 3) {
                     //Now let's interate over the columns of the current row
                     Iterator<Cell> cellIterator = row.cellIterator();
                     //Logger.getLogger(ReadExcel.class.getName()).log(Level.INFO,"Current Row Number: "+row.getRowNum());
@@ -62,24 +91,25 @@ public class ExportcsvApplication {
                         Cell cell = cellIterator.next();
                         cellValue += dataFormatter.formatCellValue(cell);
                         cellValue += ",";
+
                         //System.out.print(cellValue + ",");
                     }
                 }
 
                 //System.out.println();
+                if(rVal % 1000 == 0) {
+                    log.info("Writing Out Record: " + rVal);
+                }
+
                 bw.write(cellValue);
-                bw.newLine();
+                if(rVal > 4) {
+                    bw.newLine();
+                }
                 bw.flush();
                 cellValue = "";
-
-                /* if(rVal == 20) {
-                    bw.close();
-                    System.exit(1);
-                } */
             }
-
-
-
+            bw.close();
+            System.exit(1);
         } catch (Exception ex) {
             log.debug("General Exception thrown.  See stack trace for more additional information");
             ex.printStackTrace();
